9ccbd7848542f0e09420ff4dfb8f81c1c8c33cea
daniy <daniyil.aurora@gmail.com>
2025-11-28 19:16:01 +0100

    basic simulation

---
+++ ground-station/simulation/main.py
@@ -0,0 +1,50 @@
+import pygame as pg
+from camera import *
+from projection import *
+from object import *
+
+class SoftwareRenderer:
+    def __init__(self) -> None:
+        pg.init()
+        self.RES = self.WIDTH, self.HEIGHT = 1600, 900
+        self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
+        self.FPS = 60
+        self.screen = pg.display.set_mode(self.RES)
+        self.clock = pg.time.Clock()
+        self.create_objects()
+
+    def create_objects(self):
+        self.camera = Camera(self, [0.5, 1, -4])
+        self.projection = Projection(self)
+        self.object = self.get_object_from_file("eb_house_plant_01.obj")
+        self.object.translate([0.2, 0.4, 0.2])
+        self.object.rotate_y(math.pi / 6)
+        self.object.scale(0.05)
+
+    def get_object_from_file(self, filename):
+        vertex, faces = [], []
+        with open(filename) as f:
+            for line in f:
+                if line.startswith('v '):
+                    vertex.append([float(i) for i in line.split()[1:]] + [1])
+                elif line.startswith('f'):
+                    faces_ = line.split()[1:]
+                    faces.append([int(face_.split("/")[0]) - 1 for face_ in faces_])
+        return Object(self, vertex, faces)
+
+    def draw(self):
+        self.screen.fill(pg.Color('darkslategray'))
+        self.object.draw()
+    
+    def run(self):
+        while True:
+            self.draw()
+            self.camera.control()
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            pg.display.set_caption(str(self.clock.get_fps()))
+            pg.display.flip()
+            self.clock.tick(self.FPS)
+
+if __name__ == "__main__":
+    app = SoftwareRenderer()
+    app.run()
