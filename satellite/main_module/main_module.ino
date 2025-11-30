52fc22ef5d7137069c89bfc6c27958ea5f7886aa
daniy <daniyil.aurora@gmail.com>
2025-11-30 22:05:00 +0100

    Changed code of main module

--- satellite/main_module/main_module.ino
+++ satellite/main_module/main_module.ino
@@ -1,16 +1,68 @@
 #include "DHT.h"
 
-#define DHTPIN 2
+#define comPin 2
+#define START_COM_TIME 10
+#define HIGH_COM_TIME 20
+#define LOW_COM_TIME 50
+#define END_COM_TIME 80
+#define SPACE_COM_TIME 5
+
+#define DATA_SEPARATION_TIME 60
+
+#define DHTPIN 5
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
-int comPin = 10;
+String toBinary(int value) {
+  String result = "";
+  for (int i = 9; i >= 0; i--) {
+    result += ((value >> i) & 1) ? '1' : '0';
+  }
+  return result;
+}
+
+void sendValue(int value) {
+  // Begin communication
+  digitalWrite(comPin, HIGH);
+  delay(START_COM_TIME);
+  digitalWrite(comPin, LOW);
+    
+  // Space
+  delay(SPACE_COM_TIME);
+
+  String binary = toBinary(value);
+  
+  // Communicate value
+  for (int i = 0; i < 10; i++) {
+    if (binary[i] == '1') {
+      digitalWrite(comPin, HIGH);
+      delay(HIGH_COM_TIME);
+      digitalWrite(comPin, LOW);
+    }
+    else {
+      digitalWrite(comPin, HIGH);
+      delay(LOW_COM_TIME);
+      digitalWrite(comPin, LOW);
+    }
+    // Space
+    delay(SPACE_COM_TIME);
+  }
+
+  // Space
+  delay(SPACE_COM_TIME);
+    
+  // End communication
+  digitalWrite(comPin, HIGH);
+  delay(END_COM_TIME);
+  digitalWrite(comPin, LOW);
+  delay(SPACE_COM_TIME);
+}
 
 void setup() {
-  Serial.begin(9600);
   pinMode(comPin, OUTPUT);
-
+  digitalWrite(comPin, LOW);
+  Serial.begin(9600);
   dht.begin();
 }
 
@@ -20,39 +72,10 @@ long hum;
 void loop() {
   float rtemp = dht.readTemperature();
   float rhum = dht.readHumidity();
-
   temp = (long)(rtemp * 10);
   hum = (long)(rhum * 10);
-
-  // Starting communication
-  digitalWrite(comPin, HIGH);
-  delay(70); 
-  digitalWrite(comPin, LOW);
-  
-  // Delay to separate HIGH and LOW signals from each other
-  delay(10);
-
-  // Sending temperature
-  digitalWrite(comPin, HIGH);
-  delay(temp);
-  digitalWrite(comPin, LOW);
-
-  // Separator delay
-  delay(10);
-
-  // Sending humidity
-  digitalWrite(comPin, HIGH);
-  delay(hum);
-  digitalWrite(comPin, LOW);
-
-  // Delay again to separate HIGH and LOW
-  delay(10);
-  
-  // Ending communication
-  digitalWrite(comPin, HIGH);
-  delay(100);
-  digitalWrite(comPin, LOW);
-
-  // Last delay to separate HIGH and LOW
-  delay(10);
+  sendValue(temp);
+  delay(DATA_SEPARATION_TIME);
+  sendValue(hum);
+  delay(DATA_SEPARATION_TIME);
 }
